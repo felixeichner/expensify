98c0a79bc169b1caedab169968c78bf9e71cc627
felixeichner <eichner.f@googlemail.com>
2018-03-06 12:28:40 +0100

    Add numeral ro format currency display

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
+import 'numeral/locales/de';
+
+numeral.locale('de');
 
 const ExpenseListItem = ({ id, description, note, amount, createdAt }) => {
   return (
     <tr>
       <td><Link to={`/edit/${id}`}><strong>{description}</strong></Link></td>
       <td>{note}</td>
-      <td>€ {(amount / 100).toFixed(2)}</td>
+      <td>{numeral(amount/100).format('$ 0,0.00')}</td>
       <td>{moment(createdAt).format("DD. MMM YYYY")}</td>
     </tr>
   )
