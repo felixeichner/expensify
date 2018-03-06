7d029c74bb2104d9904a61974db2f63ea1a2a24d
felixeichner <eichner.f@googlemail.com>
2018-03-06 08:13:56 +0100

    Delete setting up dummy data in app.js

--- src/app.js
+++ src/app.js
@@ -3,40 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
-import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-import moment from 'moment';
 import 'normalize.css/normalize.css';
-import 'react-dates/lib/css/_datepicker.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(addExpense({
-  description: 'Water bill',
-  note: 'February monthly bills',
-  amount: 2000,
-  createdAt: moment("2018/02/02").valueOf()
-}));
-store.dispatch(addExpense({
-  description: 'Gas bill',
-  note: 'February monthly bills',
-  amount: 3000,
-  createdAt: moment("2018/02/04").valueOf()
-}));
-store.dispatch(addExpense({
-  description: 'Groceries',
-  note: 'February shopping',
-  amount: 4534,
-  createdAt: moment("2018/02/26").valueOf()
-}));
-store.dispatch(addExpense({
-  description: 'Shoes',
-  note: 'January shopping',
-  amount: 11000,
-  createdAt: moment("2018/01/15").valueOf()
-}));
 
 const jsx = (
   <Provider store={store}>
